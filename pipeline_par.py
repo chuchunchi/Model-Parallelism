db01545bbacc3a2c43964a8cea44af62795381a0
chuchunchi <chuchun1231@gmail.com>
2023-10-04 01:03:28 +0800

    try

--- pipeline_par.py
+++ pipeline_par.py
@@ -1,191 +1,94 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates
+import os
+import pippy
+from torch.distributed import rpc
+from torch import nn
 import torch
-from typing import Any
 import time
-import numpy as np
-class MyNetworkBlock(torch.nn.Module):
+
+RANK  = int(os.environ["RANK"])
+WORLD = int(os.environ["WORLD"])
+HOST  = os.environ["HOST"]
+PORT  = os.environ["PORT"]
+NUM_THREADS = 4
+NUM_INFERENCE = 100
+torch.set_num_threads(4)
+
+class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = torch.nn.Linear(768*4*4, 768*4*4)
-        self.fc2 = torch.nn.Linear(768*4*4, 768*4*4)
-        self.fc3 = torch.nn.Linear(768*4*4, 768*4*4)
-
+        self.fc1 = nn.Linear(768*4*4, 768*4*4)
+        self.fc2 = nn.Linear(768*4*4, 768*4*4)
+        self.fc3 = nn.Linear(768*4*4, 768*4*4)
+        # self.fc4 = nn.Linear(768, 128)
+        
     def forward(self, x):
+        # for i in range(300):
         x = self.fc1(x)
-        x = torch.nn.functional.relu(x)
+        x = nn.functional.relu(x)
         x = self.fc2(x)
-        x = torch.nn.functional.relu(x)
+        x = nn.functional.relu(x)
         x = self.fc3(x)
-        x = torch.nn.functional.relu(x)
+        x = nn.functional.relu(x)
+            # x = self.fc4(x)
         return x
 
+net = Net()
+net.eval()
 
-class MyNetwork(torch.nn.Module):
-    def __init__(self, in_dim, layer_dims):
-        super().__init__()
-
-        prev_dim = in_dim
-        for i, dim in enumerate(layer_dims):
-            setattr(self, f"layer{i}", MyNetworkBlock(prev_dim, dim))
-            prev_dim = dim
-
-        self.num_layers = len(layer_dims)
-        # 10 output classes
-        self.output_proj = torch.nn.Linear(layer_dims[-1], 10)
-
-    def forward(self, x):
-        for i in range(self.num_layers):
-            x = getattr(self, f"layer{i}")(x)
-
-        return self.output_proj(x)
+print(f"My rank is {RANK}")
 
 
-mn = MyNetwork(512, [512, 1024, 256])
-
-from pippy.IR import Pipe
-
-pipe = Pipe.from_tracing(mn)
-print(pipe)
-print(pipe.split_gm.submod_0)
-
-
-from pippy.IR import annotate_split_points, PipeSplitWrapper
-
-annotate_split_points(
-    mn,
-    {
-        "layer0": PipeSplitWrapper.SplitPoint.END,
-        "layer1": PipeSplitWrapper.SplitPoint.END,
-    },
+# first thing to do is to init RCP
+print("Waiting for all the nodes...")
+rpc.init_rpc(
+    f"worker{RANK}", # just an identifier
+    rank=RANK,
+    world_size=WORLD,
+    rpc_backend_options=rpc.TensorPipeRpcBackendOptions(
+        num_worker_threads=4,
+        rpc_timeout=500, # seconds
+        init_method=f"tcp://{HOST}:{PORT}", # head node's address and port
+    )
 )
 
-pipe = Pipe.from_tracing(mn)
-print(" pipe ".center(80, "*"))
-print(pipe)
-print(" submod0 ".center(80, "*"))
-print(pipe.split_gm.submod_0)
-print(" submod1 ".center(80, "*"))
-print(pipe.split_gm.submod_1)
-print(" submod2 ".center(80, "*"))
-print(pipe.split_gm.submod_2)
+# split the model, each process materializes its pipeline stage
+driver, stage = pippy.all_compile(
+    net,
+    num_ranks=WORLD,
+    num_chunks=WORLD, # microbatching
+    schedule="FillDrain", # feed chunks through the pipeline sequentially
+    split_policy=pippy.split_into_equal_size(WORLD), # split the model into specified number of equal-size stages
+)
 
+print(stage)
+print(f"Measuring {WORLD}-stage Pipelined elapsed time...")
+print(f"num of threads: {torch.get_num_threads()} ")
 
-# To run a distributed training job, we must launch the script in multiple
-# different processes. We are using `torchrun` to do so in this example.
-# `torchrun` defines two environment variables: `LOCAL_RANK` and `WORLD_SIZE`,
-# which represent the index of this process within the set of processes and
-# the total number of processes, respectively.
-#
-# To learn more about `torchrun`, see
-# https://pytorch.org/docs/stable/elastic/run.html
-import os
+if RANK == 0:
+    x = torch.randn(4, 768*4*4)
+    start_time = time.perf_counter()
+    for i in range(NUM_INFERENCE):
+        y = driver(x) # only rank 0 is able the call the pipeline's driver
+    end_time = time.perf_counter()
+    pipeline_time = end_time - start_time
 
-local_rank = int(os.environ["LOCAL_RANK"])
-world_size = int(os.environ["WORLD_SIZE"])
-
-# PiPPy uses the PyTorch RPC interface. To use RPC, we must call `init_rpc`
-# and inform the RPC framework of this process's rank and the total world
-# size. We can directly pass values `torchrun` provided.`
-#
-# To learn more about the PyTorch RPC framework, see
-# https://pytorch.org/docs/stable/rpc.html
-import torch.distributed.rpc as rpc
-
-rpc.init_rpc(f"worker{local_rank}", rank=local_rank, world_size=world_size, rpc_backend_options=rpc.TensorPipeRpcBackendOptions(
-         num_worker_threads=16,
-         rpc_timeout=2000 # 2000 second timeout
-    ))
-
-# PiPPy relies on the concept of a "driver" process. The driver process
-# should be a single process within the RPC group that instantiates the
-# PipelineDriver and issues commands on that object. The other processes
-# in the RPC group will receive commands from this process and execute
-# the pipeline stages
-if local_rank == 0:
-    # We are going to use the PipelineDriverFillDrain class. This class
-    # provides an interface for executing the `Pipe` in a style similar
-    # to the GPipe fill-drain schedule. To learn more about GPipe and
-    # the fill-drain schedule, see https://arxiv.org/abs/1811.06965
-    from pippy.PipelineDriver import PipelineDriverFillDrain
-    from pippy.microbatch import TensorChunkSpec
-
-    # Pipelining relies on _micro-batching_--that is--the process of
-    # dividing the program's input data into smaller chunks and
-    # feeding those chunks through the pipeline sequentially. Doing
-    # this requires that the data and operations be _separable_, i.e.
-    # there should be at least one dimension along which data can be
-    # split such that the program does not have interactions across
-    # this dimension. PiPPy provides `chunk_spec` arguments for this
-    # purpose, to specify the batch dimension for tensors in each of
-    # the args, kwargs, and outputs. The structure of the `chunk_spec`s
-    # should mirror that of the data type. Here, the program has a
-    # single tensor input and single tensor output, so we specify
-    # a single `TensorChunkSpec` instance indicating dimension 0
-    # for args[0] and the output value.
-    args_chunk_spec: Any = (TensorChunkSpec(0),)
-    kwargs_chunk_spec: Any = {}
-    output_chunk_spec: Any = TensorChunkSpec(0)
-
-    # Finally, we instantiate the PipelineDriver. We pass in the pipe,
-    # chunk specs, and world size, and the constructor will distribute
-    # our code to the processes in the RPC group. `driver` is an object
-    # we can invoke to run the pipeline.
-    driver = PipelineDriverFillDrain(
-        pipe,
-        64,
-        world_size=world_size,
-        args_chunk_spec=args_chunk_spec,
-        kwargs_chunk_spec=kwargs_chunk_spec,
-        output_chunk_spec=output_chunk_spec,
-    )
-
-    x = torch.randn(512, 512)
-
-    # Run the pipeline with input `x`. Divide the batch into 64 micro-batches
-    # and run them in parallel on the pipeline
-    output = driver(x)
-    timings = []
-    num_runs = 100
-    
-    
-    
-    with torch.no_grad():
-        for i in range(1, num_runs+1):
-            start_time = time.perf_counter()
-            reference_output = mn(x)
-            end_time = time.perf_counter()
-            timings.append(end_time - start_time)
-            if i%(num_runs/5)==0:
-                print('Iteration %d/%d, avg batch time %.2f ms'%(i, num_runs, np.mean(timings)*1000))
-    nonpipe_time = ((np.mean(timings))*1000)
-    print('Latency per query without pipeline: %.2f ms'%nonpipe_time)
-
-
-    timings = []
-    
-    
-    with torch.no_grad():
-        for i in range(1, num_runs+1):
-            start_time = time.perf_counter()
-            output = driver(x)
-            end_time = time.perf_counter()
-            timings.append(end_time - start_time)
-            if i%(num_runs/5)==0:
-                print('Iteration %d/%d, avg batch time %.2f ms'%(i, num_runs, np.mean(timings)*1000))
-    pipe_time = ((np.mean(timings))*1000)
-    print('Latency per query: %.2f ms'%pipe_time)
-    
-    # Run the original code and get the output for comparison
-    reference_output = mn(x)
-
-    # Compare numerics of pipeline and original model
-    torch.testing.assert_close(output, reference_output)
-
-    print('Communication time: ', str(pipe_time - nonpipe_time))
-
-    print('Speed up: ', str(nonpipe_time / pipe_time))
-
-    print(" Pipeline parallel model ran successfully! ".center(80, "*"))
-
-
+del net
 rpc.shutdown()
+print("rpc Bye!")
+
+print(f"Measuring Non-Pipelined elapsed time...")
+print(f"num of threads: {torch.get_num_threads()} ")
+net = Net()
+x = torch.randn(4, 768*4*4)
+start_time = time.perf_counter()
+for i in range(NUM_INFERENCE):
+    y = net(x)
+end_time = time.perf_counter()
+non_pipelined_time = end_time - start_time
+print(f"\n\n**** Result ****")
+print(f"{WORLD}-stage Pipelined Elapsed time = {pipeline_time} seconds")
+print(f"      Non-Pipelined Elapsed time = {non_pipelined_time} seconds")
+
+print(f"Speedup = {non_pipelined_time / pipeline_time}")
+print(f"No-pipelined latency = {non_pipelined_time / NUM_INFERENCE} seconds")
+print(f"Communication time = {(pipeline_time - non_pipelined_time) / NUM_INFERENCE } seconds")
