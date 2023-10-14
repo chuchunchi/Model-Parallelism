7fef7ff3a31e75e0c5449a3da76843b199eedbb4
chuchunchi <chuchun1231@gmail.com>
2023-10-14 15:06:43 +0800

    fixed setting script

--- setting.sh
+++ setting.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ls
+export MASTER_ADDR=192.168.1.100
 export MASTER_PORT=50000
-export GLOO_SOCKET_IFNAME=eth1
+export GLOO_SOCKET_IFNAME=eth0
 export WORLD_SIZE=2
-export RANK=1
+export LOCAL_RANK=1
 ls
